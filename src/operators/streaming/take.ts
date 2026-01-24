64975dbf86a4b8e26ab7c921437cc1bb4360944a
chrisitopherus <jochchristopher@gmail.com>
2026-01-24 11:48:06 +0100

    implemented taken operator

---
+++ src/operators/streaming/take.ts
@@ -0,0 +1,28 @@
+import { EnumeratorResult } from "../../core/enumeratorResult";
+import { IEnumerator } from "../../types/core";
+
+export class TakeEnumerator<T> implements IEnumerator<T> {
+    private readonly sourceEnumerator: IEnumerator<T>;
+    private readonly count: number;
+
+    private takenCount = 0;
+
+    public constructor(sourceEnumerator: IEnumerator<T>, count: number) {
+        this.sourceEnumerator = sourceEnumerator;
+        this.count = count;
+    }
+
+    public next(): IteratorResult<T> {
+        if (this.takenCount === this.count) {
+            return EnumeratorResult.done();
+        }
+
+        const result = this.sourceEnumerator.next();
+        if (result.done) {
+            return EnumeratorResult.done();
+        }
+
+        this.takenCount++;
+        return EnumeratorResult.yield(result.value);
+    }
+}
