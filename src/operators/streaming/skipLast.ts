a79f1980e78f756ed1c192b467ea22e635e3c59c
chrisitopherus <jochchristopher@gmail.com>
2026-01-24 03:31:44 +0100

    using now a ring buffer to increase performance and also fixed count

--- src/operators/streaming/skipLast.ts
+++ src/operators/streaming/skipLast.ts
@@ -5,11 +5,14 @@ import { IEnumerator } from "../../types/core";
 export class SkipLastEnumerator<T> implements IEnumerator<T> {
     private readonly sourceEnumerator: IEnumerator<T>;
     private readonly count: number;
-    private readonly queue: T[] = [];
+    private readonly buffer: T[];
+    private writeIndex: number = 0;
+    private filledCount: number = 0;
 
     public constructor(sourceEnumerator: IEnumerator<T>, count: number) {
         this.sourceEnumerator = sourceEnumerator;
-        this.count = count ? count : 0;
+        this.count = count < 0 ? 0 : count;
+        this.buffer = new Array<T>(count);
     }
 
     public next(): IteratorResult<T> {
@@ -23,12 +26,17 @@ export class SkipLastEnumerator<T> implements IEnumerator<T> {
                 return EnumeratorResult.done();
             }
 
-            this.queue.push(current.value);
-
-            if (this.queue.length > this.count) {
-                const value = this.queue.shift() as T;
-                return EnumeratorResult.yield(value);
+            if (this.filledCount < this.count) {
+                this.buffer[this.writeIndex] = current.value;
+                this.writeIndex = (this.writeIndex + 1) % this.count;
+                this.filledCount++;
+                continue;
             }
+
+            const oldest = this.buffer[this.writeIndex];
+            this.buffer[this.writeIndex] = current.value;
+            this.writeIndex = (this.writeIndex + 1) % this.count;
+            return EnumeratorResult.yield(oldest);
         }
     }
 }
