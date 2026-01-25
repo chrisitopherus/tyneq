7117060b12f83faaef2a80423cbc3d2955a7a7e4
chrisitopherus <jochchristopher@gmail.com>
2026-01-25 13:29:01 +0100

    zip now uses correct generics and made selector readonly

--- src/operators/streaming/zip.ts
+++ src/operators/streaming/zip.ts
@@ -2,10 +2,10 @@ import { TyneqEnumerator } from "../../core/enumerator";
 import { EnumeratorResult, IEnumerator } from "../../types/core";
 
 export class ZipEnumerator<T, U, V> extends TyneqEnumerator<T, V> {
-    private readonly otherEnumerator: IEnumerator<T>;
-    private selector: (first: T, second: T) => V;
+    private readonly otherEnumerator: IEnumerator<U>;
+    private readonly selector: (first: T, second: U) => V;
 
-    public constructor(sourceEnumerator: IEnumerator<T>, otherEnumerator: IEnumerator<T>, selector: (first: T, second: T) => V) {
+    public constructor(sourceEnumerator: IEnumerator<T>, otherEnumerator: IEnumerator<U>, selector: (first: T, second: U) => V) {
         super(sourceEnumerator);
         this.otherEnumerator = otherEnumerator;
         this.selector = selector;
