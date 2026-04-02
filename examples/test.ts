206cf386e0a6756ead93628e2690bd49e1660bf6
chrisitopherus <jochchristopher@gmail.com>
2026-04-03 00:07:46 +0200

    refactor: remove unused test file examples/test.ts

--- examples/test.ts
+++
@@ -1,9 +0,0 @@
-import { Tyneq, OperatorRegistry } from "../src/index";
-
-Tyneq.from([1, 2, 3]).all(x => x > 0);
-
-for (const metadata of OperatorRegistry.list()) {
-    console.log(metadata)
-}
-
-console.log(OperatorRegistry.count());
