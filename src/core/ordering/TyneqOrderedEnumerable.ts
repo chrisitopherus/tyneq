ad8cbd8e5d12e87153dddf1100ff6c9142268828
chrisitopherus <jochchristopher@gmail.com>
2026-05-16 04:08:02 +0200

    fixed comment

--- src/core/ordering/TyneqOrderedEnumerable.ts
+++ src/core/ordering/TyneqOrderedEnumerable.ts
@@ -166,7 +166,7 @@ export class TyneqOrderedEnumerable<TSource, TKey> extends TyneqEnumerableBase<T
             keySelector,
             comparer,
             descending,
-            undefined, //TODO:✅ BUG:FIX creating new root ordered enumerable, so parent is undefined CREDITS: KOMINO
+            undefined, //♥: BUG:FIX creating new root ordered enumerable, so parent is undefined CREDITS: KOMINO
             node
         );
     }
